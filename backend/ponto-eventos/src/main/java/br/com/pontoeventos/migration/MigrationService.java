eca0ba68bb58718375677442d0d7d66dff5ebd98
FariasGabriela <gabrielafarias@alunos.utfpr.edu.br>
2024-01-11 19:15:02 -0300

    alterado migration

---
+++ backend/ponto-eventos/src/main/java/br/com/pontoeventos/migration/MigrationService.java
@@ -0,0 +1,16 @@
+package br.com.pontoeventos.migration;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.flywaydb.core.Flyway;
+
+@ApplicationScoped
+public class MigrationService {
+
+    @Inject
+    Flyway flyway;
+
+    public void checkMigration() {
+        System.out.println(flyway.info().current().getVersion().toString());
+    }
+}

9a54bc11ce303ccaa8b30d76d2ea063227a91f34
FariasGabriela <gabrielafarias@alunos.utfpr.edu.br>
2024-01-13 16:46:43 -0300

    Implementado componente auto complete

---
+++ frontend/ponto-eventos/src/components/autocomplete/autocomplete.tsx
@@ -0,0 +1,42 @@
+import { TextField } from "@mui/material";
+import Autocomplete from "@mui/material/Autocomplete";
+import InstituicaoModel from "../../model/instituicao";
+import { ReactEventHandler } from "react";
+
+interface AutoCompleteProps {
+  id: string;
+  value: any;
+  title: string;
+  onChange: any;
+  selectList: InstituicaoModel[];
+  helperText: string;
+  error: boolean;
+}
+
+export default function AutoCompleteComponente(props: AutoCompleteProps) {
+  const { id, value, title, onChange, selectList, helperText, error } = props;
+
+  const defaultProps = {
+    options: selectList,
+    getOptionLabel: (option: InstituicaoModel) => option.name || "",
+  };
+
+  return (
+    <Autocomplete
+      {...defaultProps}
+      id={id}
+      value={value}
+      onChange={onChange}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+         // onBlur={onChange}
+          variant="standard"
+          helperText={helperText}
+          error={error}
+          label={title}
+        />
+      )}
+    />
+  );
+}
